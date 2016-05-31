29f3d44660d1cd8df4258c2b906a31aa0e079fd9
elizabeth <elizabeth.e.epstein@gmail.com>
2016-05-31 16:41:12 -0400

    final version

--- exercise5.rb
+++ exercise5.rb
@@ -1,8 +1,6 @@
-
+puts "Give me a temperature in F."
 num = gets.chomp
 def celsius(num)
 return (num.to_i - 32) * 5/9
 end
-puts "Give me a temperature in F:"
-num = gets.chomp
-puts celsius(num)
+puts "#{num} in C is #{celsius(num)}."
